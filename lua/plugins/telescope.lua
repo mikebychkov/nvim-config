116b32611b99e864f3f94e51c93ee3e86fe6d1e9
m.bychkov <mikhail.bychkov@octo.uz>
2024-09-04 14:32:26 +0500

    refactor plugins

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,10 @@
--- plugins/telescope.lua:
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
--- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    }
+  'nvim-telescope/telescope.nvim', 
+  tag = '0.1.8',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local telescope_builtin = require("telescope.builtin")
+    vim.keymap.set('n', '<C-p>', telescope_builtin.find_files, {})
+    vim.keymap.set('n', '<leader>fg', telescope_builtin.live_grep, {})
+  end
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,1 +1,12 @@
-return {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+return {
+  "nvim-treesitter/nvim-treesitter", 
+  build = ":TSUpdate",
+  config = function()
+    local treesitter_config = require("nvim-treesitter.configs")
+    treesitter_config.setup({
+      ensure_installed = {"lua", "vim", "java", "javascript", "typescript", "clojure"},
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end
+}

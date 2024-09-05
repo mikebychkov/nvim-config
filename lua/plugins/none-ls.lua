b4ec5b184b51a8d83422572ae9d1c8560f37c664
m.bychkov <mikhail.bychkov@octo.uz>
2024-09-05 16:34:58 +0500

    fix errors

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,8 +7,8 @@ return {
 				null_ls.builtins.formatting.stylua,
 				--null_ls.builtins.formatting.google_java_format,
 				--null_ls.builtins.formatting.clang_format,
-				null_ls.builtins.formatting.ast_lint,
-				null_ls.builtins.diagnostics.ast_lint,
+				--null_ls.builtins.formatting.ast_grep,
+				--null_ls.builtins.diagnostics.ast_grep,
 				--null_ls.builtins.diagnostics.checkstyle,
 				--null_ls.builtins.completion.spell,
 				--require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
--- lua/plugins/nvim-jdtls.lua
+++ lua/plugins/nvim-jdtls.lua
@@ -2,7 +2,7 @@ return {
 	"mfussenegger/nvim-jdtls",
 	config = function()
 		local config = {
-			cmd = { "/path/to/jdt-language-server/bin/jdtls" },
+			cmd = { "/home/mike/.local/share/nvim/mason/packages/jdtls/bin/jdtls" },
 			root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
 		}
 		require("jdtls").start_or_attach(config)

b0c60e0893badd0e4876f6518494a2a0f285f411
Mike <mihailbichkov@yandex.ru>
2024-09-08 15:38:25 +0500

    todo list

--- lua/test/env.lua
+++ lua/test/env.lua
@@ -1,8 +1,16 @@
 function Test()
     -- package.loaded.first_steps = nil
     -- require"test.first_steps".todo()
-    package.loaded.todolist = nil
+    package.loaded["test.todolist"] = nil
     require"test.todolist".todo()
+end
+
+function PrintLoaded()
+    vim.print(package.loaded)
+end
+
+function CleanLoaded()
+    package.loaded["test.todolist"] = nil
 end
 
 vim.api.nvim_create_user_command("Test", Test, {})
@@ -10,3 +18,5 @@ vim.api.nvim_create_user_command("Test", Test, {})
 -- vim.api.nvim_create_autocmd("CursorHold", { callback = require"test.first_steps".todo })
 
 vim.keymap.set("n", "<leader>T", Test, {})
+vim.keymap.set("n", "<leader>P", PrintLoaded, {})
+vim.keymap.set("n", "<leader>C", CleanLoaded, {})
--- lua/test/todolist.lua
+++ lua/test/todolist.lua
@@ -14,10 +14,28 @@ M.todo = function()
     local query = vim.treesitter.query.parse(parser:lang(), query_str)
 
     -- lua print(vim.inspect(require'vim.treesitter.query')) -- no parse_query
+    -- vim.print(query)
 
-    vim.print(query)
+    local tree = parser:parse()[1]
 
-    -- video time: 11:13
+    local qf_list = {}
+
+    for _, n in query:iter_captures(tree:root(), 0) do
+        local text = vim.treesitter.get_node_text(n, 0)
+        -- print(text)
+        local lnum, col = n:range()
+        table.insert(qf_list, {
+            bufnr = vim.api.nvim_get_current_buf(),
+            text = text,
+            lnum = lnum + 1,
+            col = col + 1
+        })
+    end
+
+    print(vim.inspect(qf_list))
+
+    vim.fn.setqflist(qf_list)
+    vim.cmd.copen()
 
 end
 

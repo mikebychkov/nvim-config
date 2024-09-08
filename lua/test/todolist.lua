15a930ee04dc3105332fe972b7950915c78c1574
m.bychkov <mikhail.bychkov@octo.uz>
2024-09-08 11:01:25 +0500

    notes

--- lua/test/todolist.lua
+++ lua/test/todolist.lua
@@ -1,5 +1,9 @@
 local M = {}
 
+-- TODO: do something
+-- another comment
+-- TODO: another thing to do
+
 M.todo = function()
 
     print("Hello new plugin world")
@@ -7,3 +11,8 @@ M.todo = function()
 end
 
 return M
+
+
+-- OPEN MANUALY:
+-- :vimgrep TODO %
+-- :copen

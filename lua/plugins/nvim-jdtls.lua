43b5104ce07d24621cae36699eb31e536ecec380
m.bychkov <mikhail.bychkov@octo.uz>
2024-12-02 14:53:23 +0500

    jtls plugin update

--- lua/plugins/nvim-jdtls.lua
+++ lua/plugins/nvim-jdtls.lua
@@ -2,7 +2,7 @@ return {
 	"mfussenegger/nvim-jdtls",
 	config = function()
 		local config = {
-			cmd = { "/home/mike/.local/share/nvim/mason/packages/jdtls/bin/jdtls" },
+			cmd = { os.getenv("HOME") .. "/.local/share/nvim/mason/packages/jdtls/bin/jdtls" },
 			root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
 		}
 		require("jdtls").start_or_attach(config)

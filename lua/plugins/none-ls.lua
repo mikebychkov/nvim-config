c0a380aafebfaf063e80a14d4b7700cedc260b3b
mike <mihailbichkov@gmail.com>
2024-12-09 17:11:51 +0500

    try to enable lombok

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,13 +5,14 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-				--null_ls.builtins.formatting.google_java_format,
-				--null_ls.builtins.formatting.clang_format,
-				--null_ls.builtins.formatting.ast_grep,
-				--null_ls.builtins.diagnostics.ast_grep,
-				--null_ls.builtins.diagnostics.checkstyle,
-				--null_ls.builtins.completion.spell,
-				--require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
+				-- null_ls.builtins.formatting.google_java_format,
+				-- null_ls.builtins.formatting.clang_format,
+				-- null_ls.builtins.formatting.ast_grep,
+				-- null_ls.builtins.diagnostics.eslint,
+				-- null_ls.builtins.diagnostics.ast_grep,
+				-- null_ls.builtins.diagnostics.checkstyle,
+				-- null_ls.builtins.completion.spell,
+				-- require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
 			},
 		})
 		vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, {})
--- lua/plugins/nvim-jdtls.lua
+++ lua/plugins/nvim-jdtls.lua
@@ -1,9 +1,36 @@
 return {
 	"mfussenegger/nvim-jdtls",
 	config = function()
+        local data_dir = vim.fn.stdpath('data')
+        local jdtls_path = data_dir .. '/mason/packages/jdtls'
+        local lombok_path = data_dir .. '/mason/packages/jdtls/lombok.jar' -- Adjust path to Lombok jar
 		local config = {
-			cmd = { os.getenv("HOME") .. "/.local/share/nvim/mason/packages/jdtls/bin/jdtls" },
+			-- cmd = { data_dir .. "/mason/packages/jdtls/bin/jdtls" },
+            cmd = {
+                'java',
+                '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+                '-Dosgi.bundles.defaultStartLevel=4',
+                '-Declipse.product=org.eclipse.jdt.ls.core.product',
+                '-Dlog.protocol=true',
+                '-Dlog.level=ALL',
+                '-javaagent:' .. lombok_path, -- <---- This is the important part to enable Lombok
+                '-Xmx1G',
+                '--add-modules=ALL-SYSTEM',
+                '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+                '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+                '-jar', jdtls_path .. '/plugins/org.eclipse.equinox.launcher_1.6.900.v20240613-2009.jar',
+                '-configuration', jdtls_path .. '/config_linux', -- Or config_win, config_mac based on your OS
+                -- '-data', workspace_dir
+                '-data', './'
+            },
 			root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
+            settings = {
+                java = {
+                    lombok = {
+                        enabled = true
+                    }
+                }
+            }
 		}
 		require("jdtls").start_or_attach(config)
 	end,

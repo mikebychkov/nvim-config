590d6999261eca5897c85d282b0bbfaf9e990a71
m.bychkov <mikhail.bychkov@octo.uz>
2025-06-05 15:06:22 +0500

    fg-update

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,7 +6,13 @@ return {
 		config = function()
 			local telescope_builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<C-p>", telescope_builtin.find_files, {})
-			vim.keymap.set("n", "<leader>fg", telescope_builtin.live_grep, {})
+			-- vim.keymap.set("n", "<leader>fg", telescope_builtin.live_grep, {})
+
+			vim.keymap.set("n", "<leader>fg", function()
+                local my_curr_word = vim.fn.expand('<cword>')
+                telescope_builtin.live_grep({ default_text = my_curr_word })
+                -- telescope_builtin.live_grep({ default_text = 'hellomf' })
+            end, {})
 		end,
 	},
 	{

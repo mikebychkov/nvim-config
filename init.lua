6013a79549d6d65b18ebbff4a2878456a94cbf00
m.bychkov <mikhail.bychkov@octo.uz>
2025-06-05 14:13:23 +0500

    transparency

--- init.lua
+++ init.lua
@@ -2,3 +2,31 @@ require"vim-options"
 require"config.lazy"
 require"test.env"
 require"globals"
+
+vim.cmd [[
+  highlight Normal guibg=NONE ctermbg=NONE
+  highlight NormalNC guibg=NONE ctermbg=NONE
+  highlight EndOfBuffer guibg=NONE ctermbg=NONE
+]]
+
+vim.cmd [[
+  highlight LineNr guibg=NONE ctermbg=NONE
+  highlight SignColumn guibg=NONE ctermbg=NONE
+  highlight VertSplit guibg=NONE ctermbg=NONE
+]]
+
+vim.cmd [[
+  highlight Normal guibg=NONE ctermbg=NONE
+]]
+
+-- Make Neo-tree transparent
+vim.cmd [[
+  highlight NeoTreeNormal guibg=NONE ctermbg=NONE
+  highlight NeoTreeNormalNC guibg=NONE ctermbg=NONE
+  highlight NeoTreeEndOfBuffer guibg=NONE ctermbg=NONE
+]]
+
+vim.cmd [[
+  highlight NeoTreeSignColumn guibg=NONE ctermbg=NONE
+  highlight NeoTreeWinSeparator guibg=NONE ctermbg=NONE
+]]

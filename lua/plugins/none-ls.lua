c30401a867b1c689ba7e26dd0e15fa18a1a0ff15
m.bychkov <mikhail.bychkov@octo.uz>
2025-06-17 13:49:26 +0500

    update

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,6 +6,7 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.goimports,
+				null_ls.builtins.formatting.biome,
 				-- null_ls.builtins.formatting.google_java_format,
 				-- null_ls.builtins.formatting.clang_format,
 				-- null_ls.builtins.formatting.ast_grep,

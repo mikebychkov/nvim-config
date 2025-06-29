e276fce998da379b8387c24dac2183a8e2c3bf86
mikebychkov <mikebychkov@github.com>
2025-06-29 21:39:18 +0500

    update

--- lua/globals.lua
+++ lua/globals.lua
@@ -1,5 +1,6 @@
 vim.keymap.set("n", "<leader>b", ":buffers<CR>")
 vim.keymap.set("n", "<leader>bn", ":bnext<CR>")
+vim.keymap.set("n", "<leader>bb", ":bprev<CR>")
 
 P = function(v)
     print(vim.inspect(v))

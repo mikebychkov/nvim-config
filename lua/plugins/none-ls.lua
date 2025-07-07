373159e365595ba97801311747511cbb822e634f
m.bychkov <mikhail.bychkov@octo.uz>
2025-07-07 18:15:25 +0500

    update

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,6 +7,7 @@ return {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.goimports,
 				null_ls.builtins.formatting.biome,
+				null_ls.builtins.diagnostics.pylint,
 				-- null_ls.builtins.formatting.google_java_format,
 				-- null_ls.builtins.formatting.clang_format,
 				-- null_ls.builtins.formatting.ast_grep,
@@ -18,5 +19,6 @@ return {
 			},
 		})
 		vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, {})
+		vim.keymap.set("n", "<leader>fd", vim.diagnostic.open_float, {})
 	end,
 }
